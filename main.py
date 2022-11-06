aa8daf7ef5e7a90fa2fe20557b38ce826bfafee9
HatsulaSeveryn <104780480+HatsulaSeveryn@users.noreply.github.com>
2022-11-06 19:03:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,240 @@
+from collections import UserDict
+from datetime import date
+import re
+import pickle
+
+
+def input_error(message: str = ''):
+    """
+    Decorator function for handling input errors
+    :param message: optional parameter to specify the input error
+    """
+
+    def inner(handler):
+        def wrapper(*args, **kwargs):
+            try:
+                return handler(*args, **kwargs)
+            except Exception as error_:
+                print(error_, '\n', message)
+
+        return wrapper
+
+    return inner
+
+
+class Field:
+
+    def __init__(self, value):
+        self._value = str(value)
+
+    def __repr__(self):
+        return self._value
+
+    def __str__(self):
+        return self._value
+
+    def __eq__(self, other):
+        return self._value == other.value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    @input_error('Wrong phone number format, try: {+}{10-15 digits without spaces or other symbols}')
+    def value(self, phone_num):
+        if not phone_num:
+            pass
+        else:
+            if re.fullmatch(r'[+]?\d{10,15}', phone_num.strip()):
+                self._value = phone_num.strip()
+            else:
+                raise ValueError
+
+
+class Birthday(Field):
+
+    def __init__(self, value):
+        self._value = str(value)
+        self.year_ = int(value.split('.')[0])
+        self.month_ = int(value.split('.')[1])
+        self.day_ = int(value.split('.')[2])
+        super().__init__(self)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    @input_error('Wrong birthday format, try: {yyyy.mm.dd}')
+    def value(self, value):
+        if not value:
+            pass
+        else:
+            if re.fullmatch(r'\d{4}[.]\d{2}[.]\d{2}', value.strip()):
+                self._value = value.strip()
+            else:
+                raise ValueError
+
+
+class Record:
+
+    def __init__(self, name, *args):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = Birthday('1900.01.01')
+
+    @input_error()
+    def add_phone(self, phone, *args):
+        print(f'{phone} has been added to the {self.name} record')
+        return self.phones.append(Phone(phone))
+
+    @input_error('Phone not found!')
+    def delete_phone(self, phone, *args):
+        print(f'{phone} has been deleted from the {self.name} record')
+        return self.phones.remove(Phone(phone))
+
+    @input_error('Incorrect phone number when trying to update!')
+    def change_phone(self, old_phone, new_phone, *args):
+        self.delete_phone(old_phone)
+        self.add_phone(new_phone)
+
+    @input_error()
+    def add_birthday(self, birthday, *args):
+        print(f'{self.name} birthday has been added')
+        self.birthday.value = birthday
+
+    @input_error()
+    def days_to_birthday(self, *args):
+        if self.birthday:
+            today_ = date.today()
+            birthday_ = date(today_.year, self.birthday.month_, self.birthday.day_)
+            if today_ == birthday_:
+                print('Happy birthday!')
+            else:
+                if birthday_ < today_:
+                    birthday_ = birthday_.replace(year=today_.year + 1)
+                print(f'Days until birthday: {birthday_ - today_}')
+
+    def __repr__(self):
+        return f'{self.name}: phones {self.phones}, birthday {self.birthday}'
+
+
+class AddressBook(UserDict):
+
+    @input_error()
+    def add_record(self, record, *args):
+        if record.name._value not in self.data:
+            self.data[record.name._value] = record
+            print(f'{record} has been added')
+        else:
+            print(f'User with name {record.name._value} already exist')
+
+    @input_error('Update record: update {name} add/delete/change {phone} {new phone}(optional)')
+    def update_record(self, record, option, *phones):
+        if record.name._value in self.data:
+            match option:
+                case 'add':
+                    self.data[record.name._value].add_phone(Phone(phones[0]))
+                case 'delete':
+                    self.data[record.name._value].delete_phone(Phone(phones[0]))
+                case 'change':
+                    self.data[record.name._value].change_phone(Phone(phones[0]), Phone(phones[1]))
+        else:
+            print(f"Record with name {record.name} doesn't exist!")
+            raise NameError
+
+    @input_error('Unable to delete. Record not found!')
+    def delete_record(self, record, *args):
+        print(f'Record {record} has been deleted!')
+        del self.data[record.name._value]
+
+    @input_error()
+    def show_phones(self, name):
+        print(self.data[name])
+
+    @input_error()
+    def show_all(self):
+        print(self.__repr__())
+
+    @input_error()
+    def iterator(self, count: int):
+        i = 0
+        while i < count:
+            yield list(self.data.items())[i]
+            i += 1
+
+    @input_error()
+    def show_n_rec(self, count):
+        for rec in self.iterator(count):
+            print(rec)
+
+    def write_contacts_to_file(self):
+        with open('data.pickle', 'wb') as file:
+            pickle.dump(self, file)
+
+    def read_contacts_from_file(self):
+        try:
+            with open('data.pickle', 'rb') as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            pass
+
+    def search(self, word):
+        found_list = []
+        for rec in self:
+            if word in rec.name:
+                found_list.append(rec.name)
+                continue
+            if rec.phones:
+                for phone in rec.phones:
+                    if word in phone:
+                        found_list.append(rec.name)
+                        continue
+        print(found_list)
+
+    def __repr__(self):
+        return self.data
+
+
+def main():
+    book = AddressBook()
+    book.read_contacts_from_file()
+
+    while True:
+        user_command = (input('...').lower()).split()
+        match user_command[0]:
+            case 'add':
+                book.add_record(Record(*user_command[1:]))
+            case 'update':
+                book.update_record(Record(user_command[1]), user_command[2], *user_command[3:])
+            case 'delete':
+                book.delete_record(Record(*user_command[1:]))
+            case 'birthday':
+                book[user_command[1]].add_birthday(user_command[2])
+            case 'days':
+                book[user_command[1]].days_to_birthday()
+            case 'phone':
+                book.show_phones(Record(*user_command[1:]))
+            case 'show':
+                book.show_all()
+            case 'records':
+                book.show_n_rec(int(user_command[1]))
+            case 'search':
+                book.search(user_command[1])
+            case 'close' | 'exit' | 'quit':
+                book.write_contacts_to_file()
+                quit()
+            case _:
+                print('Wrong command!')
+
+
+if __name__ == '__main__':
+    main()
